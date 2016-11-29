c60466f8e44d1c624f90e64d41446308701bcac4
Matt Ephraim <me@mattephraim.com>
2016-11-28 21:28:13 -0600

    Cleanup the MembersStore service a little bit
    Make it so that it's not so awkward to wrap the method calls with a
    promise.

--- site/js/angular/services/members_store_service.js
+++ site/js/angular/services/members_store_service.js
@@ -8,8 +8,11 @@ var MembersStore = require('../../lib/members_store.js');
  */
 function MembersStoreService($http, $q) {
   var store;
-  var httpPromise = $http.get('/data/congress_members.json');
-  httpPromise.then(function(response) {
+
+  // Make request for congress members JSON data as soon as the app loads.
+  // Store the HTTP promise for the data so that it can be used in `getJSON`.
+  var congressMembersHttpPromise = $http.get('/data/congress_members.json');
+  congressMembersHttpPromise.then(function(response) {
     store = new MembersStore(response.data);
   });
 
@@ -17,124 +20,127 @@ function MembersStoreService($http, $q) {
     ROLE_TYPE_REPRESENTATIVE: MembersStore.ROLE_TYPE_REPRESENTATIVE,
     ROLE_TYPE_SENATOR: MembersStore.ROLE_TYPE_SENATOR,
 
-    /**
-     * Find a member by their govtrack ID.
-     *
-     * @param {Number} id
-     * @returns {Promise}
-     */
-    findById: function(id) {
-      return $q(function(resolve, reject) {
-        if (store) {
-          resolve(store.findById(id));
-        } else {
-          httpPromise.then(function() {
-            resolve(store.findById(id));
-          });
-        }
-      });
-    },
-
-    /**
-     * Find all senators or representatives for a state
-     *
-     * @param {String} state
-     * @param {String?} roleType
-     * @returns {Promise}
-     */
-    findAllForState: function(state, roleType) {
-      return $q(function(resolve, reject) {
-        if (store) {
-          resolve(store.findAllForState(state, roleType));
-        } else {
-          httpPromise.then(function() {
-            resolve(store.findAllForState(state, roleType));
-          });
-        }
-      });
-    },
-
-    /**
-     * Find the senators and representatives for a district.
-     *
-     * @param {String} state
-     * @param {Number} district
-     * @returns {Promise}
-     */
-    findAllForDistrict: function(state, district) {
-      return $q(function(resolve, reject) {
-        if (store) {
-          resolve(store.findAllForDistrict(state, district));
-        } else {
-          httpPromise.then(function() {
-            resolve(store.findAllForDistrict(state, district));
-          });
-        }
-      });
-    },
-
-    /**
-     * Find the representative for a specific district.
-     *
-     * @param {String} state
-     * @param {Number} district
-     * @returns {Promise}
-     */
-    findRepresentativeForDistrict: function(state, district) {
-      return $q(function(resolve, reject) {
-        if (store) {
-          resolve(store.findRepresentativeForDistrict(state, district));
-        } else {
-          httpPromise.then(function() {
-            resolve(store.findRepresentativeForDistrict(state, district));
-          });
-        }
-      });
-    },
-
-    /**
-     * Can this query be used to search for an address?
-     * The Geolocation API requires a minimum number of words to successfully search
-     * for an address.
-     *
-     * @param {String} query
-     * @returns {Boolean}
-     */
-    isValidAddressSearch: function(query) {
-      return MembersStore.isValidAddressSearch(query);
-    },
-
-    /**
-     * Can this query be used to search for a name?
-     *
-     * Some search strings are going to be very slow to search using the Fuse library
-     * so restrict the length of names that will be searched using it.
-     *
-     * @param {String} query
-     * @returns {Boolean}
-     */
-    isValidNameSearch: function(query) {
-      return MembersStore.isValidNameSearch(query);
-    },
-
-    /**
-     * Search for a member for congress.
-     *
-     * @param {String} query
-     * @returns {Promise}
-     */
-    search: function(query) {
-      return $q(function(resolve, reject) {
-        if (store) {
-          resolve(store.search(query));
-        } else {
-          httpPromise.then(function() {
-            resolve(store.search(query));
-          });
-        }
-      });
-    }
+    findById: findById,
+    findAllForState: findAllForState,
+    findAllForDistrict: findAllForDistrict,
+    findRepresentativeForDistrict: findRepresentativeForDistrict,
+    isValidAddressSearch: isValidAddressSearch,
+    isValidNameSearch: isValidNameSearch,
+    search: search
   };
+
+  /**
+   * Find a member by their govtrack ID.
+   *
+   * @param {Number} id
+   * @returns {Promise}
+   */
+  function findById(id) {
+    return $q(function(resolve) {
+      getStore().then(function(store) {
+        resolve(store.findById(id));
+      });
+    });
+  }
+
+  /**
+   * Find all senators or representatives for a state
+   *
+   * @param {String} state
+   * @param {String?} roleType
+   * @returns {Promise}
+   */
+  function findAllForState(state, roleType) {
+    return $q(function(resolve, reject) {
+      getStore().then(function(store) {
+        resolve(store.findAllForState(state, roleType));
+      });
+    });
+  }
+
+  /**
+   * Find the senators and representatives for a district.
+   *
+   * @param {String} state
+   * @param {Number} district
+   * @returns {Promise}
+   */
+  function findAllForDistrict(state, district) {
+    return $q(function(resolve, reject) {
+      getStore().then(function(store) {
+        resolve(store.findAllForDistrict(state, district));
+      });
+    });
+  }
+
+  /**
+   * Find the representative for a specific district.
+   *
+   * @param {String} state
+   * @param {Number} district
+   * @returns {Promise}
+   */
+  function findRepresentativeForDistrict(state, district) {
+    return $q(function(resolve, reject) {
+      getStore().then(function(store) {
+        resolve(store.findRepresentativeForDistrict(state, district));
+      });
+    });
+  }
+
+  /**
+   * Wrapper function for getting the initialized members store instance.
+   *
+   * If any of the service methods are called before the JSON file has fully
+   * loaded, this will guarantee that the methods eventually resolve when the
+   * JSON file loads.
+   */
+  function getStore() {
+    return $q(function(resolve) {
+      congressMembersHttpPromise.then(function() {
+        resolve(store);
+      });
+    });
+  }
+
+  /**
+   * Can this query be used to search for an address?
+   * The Geolocation API requires a minimum number of words to successfully search
+   * for an address.
+   *
+   * @param {String} query
+   * @returns {Boolean}
+   */
+  function isValidAddressSearch(query) {
+    return MembersStore.isValidAddressSearch(query);
+  }
+
+  /**
+   * Can this query be used to search for a name?
+   *
+   * Some search strings are going to be very slow to search using the Fuse library
+   * so restrict the length of names that will be searched using it.
+   *
+   * @param {String} query
+   * @returns {Boolean}
+   */
+  function isValidNameSearch(query) {
+    return MembersStore.isValidNameSearch(query);
+  }
+
+  /**
+   * Search for a member for congress.
+   *
+   * @param {String} query
+   * @returns {Promise}
+   */
+  function search(query) {
+    return $q(function(resolve, reject) {
+      getStore().then(function(store) {
+        resolve(store.search(query));
+      });
+    });
+  }
 }
 
 module.exports = MembersStoreService;
