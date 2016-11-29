ab3521ee1c12d6b333b0809d80c87250a149e588
Matt Ephraim <me@mattephraim.com>
2016-11-28 18:42:22 -0600

    Add a script for parsing the congressional distrcts data file
    Parse the congressional districts data file and output a JSON file
    that can be used to query display congressional district information
    (e.g. zipcodes for districts).

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 var govTrack = require('govtrack-node');
 var congressMembers = require('./scripts/congress_members.js');
+var congressionalDistricts = require('./scripts/congressional_districts.js');
 
 module.exports = function(grunt) {
   grunt.initConfig({
@@ -10,6 +11,14 @@ module.exports = function(grunt) {
     var done = this.async();
     congressMembers.getDataFromAPI().then(function(data) {
       grunt.file.write('data/congress_members.json', JSON.stringify(data));
+      done();
+    });
+  });
+
+  grunt.registerTask('indexDistricts', 'converts the tab delimited list of congressional districts to a JSON file', function() {
+    var done = this.async();
+    congressionalDistricts.getDistrictDataFromCsv().then(function(data) {
+      grunt.file.write('data/congressional_districts.json', JSON.stringify(data));
       done();
     });
   });
---
+++ scripts/congressional_districts.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var Baby = require('babyparse');
+var Promise = require('promise');
+
+module.exports = {
+  getDistrictDataFromCsv: getDistrictDataFromCsv
+};
+
+/**
+ * Parses the congressional district file from the census site and returns a formatted
+ * JSON file for the app.
+ *
+ * The congressional district file is the 115th congressional district file
+ * downloaded from:
+ *
+ * http://www.census.gov/geo/maps-data/data/gazetteer2016.html
+ *
+ * @returns {Promise}
+ */
+function getDistrictDataFromCsv() {
+  return new Promise(function(resolve, reject) {
+    fs.readFile('data/congressional_districts.txt', 'utf8', function(err, data) {
+      resolve(Baby.parse(data, {
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: true
+      }).data.map(function(result) {
+        return _formatResultForJSON(result);
+      }));
+    });
+  });
+}
+
+/**
+ * Formats a result from the parsed congressional district file as object for
+ * our app.
+ *
+ * @private
+ * @param {Object[]}
+ * @returns {Object[]}
+ */
+function _formatResultForJSON(result) {
+  return {
+    // GEOD is the state FIPS ID + the Congressional District ID
+    district: parseInt(result.GEOID.toString().substr(2)),
+    lat: result.INTPTLAT,
+    lng: result.INTPTLONG,
+    state: result.USPS 
+  };
+}
